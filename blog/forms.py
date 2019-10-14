22de48a9ec7ca352919b3481d2b40f1ecaa16184
Shreya Gupta <51705629+shryagupta@users.noreply.github.com>
2019-10-14 23:23:09 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,18 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+        
+class SignUpForm(UserCreationForm):
+    name = forms.CharField(max_length=30 , required=False)    
+    blood_group = forms.CharField(max_length=1,required=True)
+    email = forms.EmailField(max_length=254)
+    address =forms.CharField(max_length=100)
+    city =forms.CharField(max_length =100)
+    about_me =forms.CharField(max_length=1000)
+    
+
+    class meta:
+        model =User
+        fields =('name','email','blood_group','address','city','password2','about_me')
